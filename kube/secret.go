9bfe154bcf2bf5f6178ecf9e6e23d4141a5d2093
logica0419 <logica0419@gmail.com>
2025-03-27 21:24:58 +0900

    add kube/secret operations

--- kube/secret.go
+++ kube/secret.go
@@ -1,1 +1,39 @@
 package kube
+
+import (
+	"context"
+
+	coreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func GetSecret(ctx context.Context, name, namespace string) (*coreV1.Secret, error) {
+	secret, err := clientSet.CoreV1().Secrets(namespace).Get(ctx, name, metaV1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+}
+
+func ApplySecret(ctx context.Context, secret *coreV1.Secret) error {
+	_, err := clientSet.CoreV1().Secrets(secret.Namespace).Get(ctx, secret.Name, metaV1.GetOptions{})
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	if err != nil {
+		_, err = clientSet.CoreV1().Secrets(secret.Namespace).Create(ctx, secret, metaV1.CreateOptions{})
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err = clientSet.CoreV1().Secrets(secret.Namespace).Update(ctx, secret, metaV1.UpdateOptions{})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
