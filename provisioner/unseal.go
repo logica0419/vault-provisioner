1854387702ae2494725bb54026d2140258bc2451
logica0419 <logica0419@gmail.com>
2025-03-28 21:12:55 +0900

    refactor unseal.go

--- provisioner/unseal.go
+++ provisioner/unseal.go
@@ -17,6 +17,67 @@ type UnsealOption struct {
 	Threshold int `json:"threshold" mapstructure:"threshold" yaml:"threshold"`
 }
 
+func (p *Provisioner) Unseal(ctx context.Context) error {
+	initializedStatus, sealedStatus, err := p.getSealStatus(ctx)
+	if err != nil {
+		return err
+	}
+
+	if !slices.Contains(initializedStatus, true) {
+		res, err := p.vaultClients[0].Sys().InitWithContext(ctx, &vault.InitRequest{
+			SecretShares:    p.unsealOpt.Share,
+			SecretThreshold: p.unsealOpt.Threshold,
+			StoredShares:    p.unsealOpt.Share,
+		})
+		if err != nil {
+			return err
+		}
+
+		slog.Info("Initialized Vault", slog.Int("instance", 0))
+
+		initializedStatus[0] = true
+
+		err = p.keyStorage.Store(ctx, res.RootToken, res.Keys)
+		if err != nil {
+			return err
+		}
+	}
+
+	rootToken, keys, err := p.keyStorage.Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	if initializedStatus[0] && sealedStatus[0] {
+		err = p.unsealSingleInstance(ctx, 0, keys)
+		if err != nil {
+			return err
+		}
+
+		sealedStatus[0] = false
+	}
+
+	err = p.joinAllInstances(ctx, initializedStatus)
+	if err != nil {
+		return err
+	}
+
+	p.authenticate(rootToken)
+
+	for i := range p.vaultClients {
+		if !sealedStatus[i] {
+			continue
+		}
+
+		err = p.unsealSingleInstance(ctx, i, keys)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (p *Provisioner) getSealStatus(ctx context.Context) ([]bool, []bool, error) {
 	initializedStatus := make([]bool, len(p.vaultClients))
 	sealedStatus := make([]bool, len(p.vaultClients))
@@ -80,67 +141,6 @@ func (p *Provisioner) unsealSingleInstance(ctx context.Context, idx int, keys []
 	}
 
 	slog.Info("Unsealed Vault", slog.Int("instance", idx))
-
-	return nil
-}
-
-func (p *Provisioner) Unseal(ctx context.Context) error {
-	initializedStatus, sealedStatus, err := p.getSealStatus(ctx)
-	if err != nil {
-		return err
-	}
-
-	if !slices.Contains(initializedStatus, true) {
-		res, err := p.vaultClients[0].Sys().InitWithContext(ctx, &vault.InitRequest{
-			SecretShares:    p.unsealOpt.Share,
-			SecretThreshold: p.unsealOpt.Threshold,
-			StoredShares:    p.unsealOpt.Share,
-		})
-		if err != nil {
-			return err
-		}
-
-		slog.Info("Initialized Vault", slog.Int("instance", 0))
-
-		initializedStatus[0] = true
-
-		err = p.keyStorage.Store(ctx, res.RootToken, res.Keys)
-		if err != nil {
-			return err
-		}
-	}
-
-	rootToken, keys, err := p.keyStorage.Get(ctx)
-	if err != nil {
-		return err
-	}
-
-	if initializedStatus[0] && sealedStatus[0] {
-		err = p.unsealSingleInstance(ctx, 0, keys)
-		if err != nil {
-			return err
-		}
-
-		sealedStatus[0] = false
-	}
-
-	err = p.joinAllInstances(ctx, initializedStatus)
-	if err != nil {
-		return err
-	}
-
-	p.authenticate(rootToken)
-
-	for i := range p.vaultClients {
-		if !sealedStatus[i] {
-			continue
-		}
-
-		err = p.unsealSingleInstance(ctx, i, keys)
-		if err != nil {
-			return err
-		}
-	}
 
 	return nil
 }
